9e6a6ff6e2592133155b97fae615aeb1957a04b2
Adir Amsalem <adira@wix.com>
2020-12-28 14:04:49 +0200

    Support "query" in api-mocks (#8)

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,10 @@ export {
   TestDriver,
   driverFactory,
 } from './test-driver/test-driver';
+export {
+  Method,
+  ApiMock,
+  ApiMockRequestBuilder,
+  ApiMockResponseBuilder,
+  createApiMock,
+} from './api-mocks';
